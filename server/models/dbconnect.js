c8fd816811502fda51cfb02afd2d3f368aec6dd9
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-12 19:39:44 -0800

    mongodb pointer and log location

--- server/models/dbconnect.js
+++ server/models/dbconnect.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI_TEST || 'mongodb://localhost/spotifynd';
+mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI_SPOTIFYND || 'mongodb://localhost/spotifynd';
 mongoose.connect(mongoURI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
- console.log('Mongodb connection open');
+ console.log('Mongodb connection open for ' + mongoURI);
 });
 
 module.exports = db;
