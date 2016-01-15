73cd2da713377289ea760db6fa15420d4fb530c9
ZACK DEAN <vanleuvenze@gmail.com>
2016-01-14 19:23:39 -0800

    merge conflict resolution

--- server/routers/router.js
+++ server/routers/router.js
@@ -13,24 +13,28 @@ module.exports = function(app, express) {
   app.get('/api/trips/:id', controller.accessTrip);
   app.get('/api/trips', controller.getAllTrips);
 
+
+  //get this route working so that we can find all trips associated with one user.
+  app.get('/api/user/trips/*', userController.findAllUserTrips);
+
+
  //###### Live but not used in production############
   app.get('/api/user/*', userController.findUser);
   app.put('/api/user/*', userController.addTrips);
-  app.get('/api/user/trips/*', userController.findAllUserTrips);
 
-  function restrict(req, res, next) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-    if (req.session.user) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-      next();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-    } else {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-      req.session.error = 'Access denied!';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-      res.redirect('/api/login');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+  function restrict(req, res, next) { 
+    if (req.session.user) {
+      next();
+    } else {                  
+      req.session.error = 'Access denied!';
+      res.redirect('/api/login');
+    }                    
   }    
 
 
   //############Pending Routes#####################
   // app.get('/api/user/*/*', userController.findOneUserTrip);
-  //app.get('/user', controller.checkAuth);
+  // app.get('/user', controller.checkAuth);
   // app.get('/activities/*', controller.fetchCityData);
   // app.get('/db/activities', dbController.retrieveActivities);
   // app.post('/db/activities', dbController.storeActivities);
